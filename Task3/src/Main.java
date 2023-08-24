084eb6089e529d7f60aa7afae545ecded16d2222
dem0N4k <demON4k1993@gmail.com>
2023-08-24 22:49:14 +0300

    finish

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public static final String VALID = "()";
+    private static final String VALID = "((([А-яёЁ]+)|([А-яёЁ-]+))\\s){2}(([А-яёЁ]+)|([А-яёЁ-]+))";
 
     public static void main(String[] args) {
-       // String man = new Scanner(System.in).nextLine().trim();
+        System.out.println("Введите ФИО\n");
+        //String man = new Scanner(System.in).nextLine().trim();
         String man = "Иванов Иван Иванович";
-        String lastName = man.substring(0, man.indexOf(' ')).trim();
-        String firstName = man.substring(man.indexOf(' '), man.lastIndexOf(' ')).trim();
-        String fatherName = man.substring(man.lastIndexOf(' ')).trim();
-        System.out.println("Фамилия: " + lastName +
-                             "\nИмя: " + firstName +
-                            "\nОтчество: " + fatherName);
+
+        if (man.matches(VALID)) {
+           /* String lastName = man.substring(0, man.indexOf(' ')).trim();
+            String firstName = man.substring(man.indexOf(' '), man.lastIndexOf(' ')).trim();
+            String fatherName = man.substring(man.lastIndexOf(' ')).trim();*/
+
+            String[] split = man.split("\\s");
+            System.out.println("Фамилия: " + split[0] +
+                    "\nИмя: " + split[1] +
+                    "\nОтчество: " + split[2]);
+        } else {
+            System.out.println("Введенная строка не является ФИО");
+        }
+
+
     }
 }
--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
+    private static String VALID_FIRST = "(\\d{11})";
+    private static String VALID_SECOND = "(\\d{10})";
+    private static String NUMBER = "(\\d{1})(\\d{3})(\\d{3})(\\d{2})(\\d{2})";
+    private static String NUMBER_FORMAT = "+$1 ($2) $3 $4-$5";
+
     public static void main(String[] args) {
-        //todo Numbers
+        metod();
+    }
+
+    public static void metod() {
+        System.out.println("Введите номер телефона\n");
+        String number = new Scanner(System.in).nextLine();
+        //String number = "8-905-1234567";
+        number = number.replaceAll("\\D", "");
+        //System.out.println(number + "\n");
+        if (number.matches(VALID_FIRST)) {                   // (number.length()) == 11)
+            if (number.charAt(0) == '7') {
+                print(number);
+            } else if (number.charAt(0) == '8') {
+                number = number.replaceFirst("8", "7");
+                print(number);
+            } else {
+                error();
+            }
+        } else if (number.matches(VALID_SECOND)) {             // (number.length()) == 10)
+            number = "7" + number;
+            print(number);
+        } else {
+            error();
+        }
+    }
+
+    public static void error() {
+        System.out.println("Неверный формат номера");
+    }
+
+    public static void print(String number) {
+        number = number.replaceAll(NUMBER, NUMBER_FORMAT);
+        System.out.println("Номер " + number + " сохранен");
     }
 }
